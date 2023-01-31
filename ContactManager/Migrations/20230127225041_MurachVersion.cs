5f690b46c1acfbd62bb1456d36dc4f913a4f20b1
Jeff Scott <jpscott@ranken.edu>
2023-01-31 13:27:14 -0600

    Murach Copy of Contact Manager App

---
+++ ContactManager/Migrations/20230127225041_MurachVersion.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ContactManager.Migrations
+{
+    public partial class MurachVersion : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Contacts",
+                keyColumn: "ContactId",
+                keyValue: 1,
+                column: "DateAdded",
+                value: new DateTime(2023, 1, 27, 16, 50, 41, 484, DateTimeKind.Local).AddTicks(4366));
+
+            migrationBuilder.UpdateData(
+                table: "Contacts",
+                keyColumn: "ContactId",
+                keyValue: 2,
+                column: "DateAdded",
+                value: new DateTime(2023, 1, 27, 16, 50, 41, 486, DateTimeKind.Local).AddTicks(6720));
+
+            migrationBuilder.UpdateData(
+                table: "Contacts",
+                keyColumn: "ContactId",
+                keyValue: 3,
+                column: "DateAdded",
+                value: new DateTime(2023, 1, 27, 16, 50, 41, 486, DateTimeKind.Local).AddTicks(6757));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Contacts",
+                keyColumn: "ContactId",
+                keyValue: 1,
+                column: "DateAdded",
+                value: new DateTime(2019, 12, 19, 18, 2, 51, 201, DateTimeKind.Local).AddTicks(5117));
+
+            migrationBuilder.UpdateData(
+                table: "Contacts",
+                keyColumn: "ContactId",
+                keyValue: 2,
+                column: "DateAdded",
+                value: new DateTime(2019, 12, 19, 18, 2, 51, 203, DateTimeKind.Local).AddTicks(5117));
+
+            migrationBuilder.UpdateData(
+                table: "Contacts",
+                keyColumn: "ContactId",
+                keyValue: 3,
+                column: "DateAdded",
+                value: new DateTime(2019, 12, 19, 18, 2, 51, 203, DateTimeKind.Local).AddTicks(5117));
+        }
+    }
+}
---
+++ ContactManager/Migrations/20230131184823_JPSInitialSetup.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ContactManager.Migrations
+{
+    public partial class JPSInitialSetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Contacts",
+                keyColumn: "ContactId",
+                keyValue: 1,
+                column: "DateAdded",
+                value: new DateTime(2023, 1, 31, 12, 48, 23, 9, DateTimeKind.Local).AddTicks(3677));
+
+            migrationBuilder.UpdateData(
+                table: "Contacts",
+                keyColumn: "ContactId",
+                keyValue: 2,
+                column: "DateAdded",
+                value: new DateTime(2023, 1, 31, 12, 48, 23, 12, DateTimeKind.Local).AddTicks(3704));
+
+            migrationBuilder.UpdateData(
+                table: "Contacts",
+                keyColumn: "ContactId",
+                keyValue: 3,
+                column: "DateAdded",
+                value: new DateTime(2023, 1, 31, 12, 48, 23, 12, DateTimeKind.Local).AddTicks(3749));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Contacts",
+                keyColumn: "ContactId",
+                keyValue: 1,
+                column: "DateAdded",
+                value: new DateTime(2023, 1, 27, 16, 50, 41, 484, DateTimeKind.Local).AddTicks(4366));
+
+            migrationBuilder.UpdateData(
+                table: "Contacts",
+                keyColumn: "ContactId",
+                keyValue: 2,
+                column: "DateAdded",
+                value: new DateTime(2023, 1, 27, 16, 50, 41, 486, DateTimeKind.Local).AddTicks(6720));
+
+            migrationBuilder.UpdateData(
+                table: "Contacts",
+                keyColumn: "ContactId",
+                keyValue: 3,
+                column: "DateAdded",
+                value: new DateTime(2023, 1, 27, 16, 50, 41, 486, DateTimeKind.Local).AddTicks(6757));
+        }
+    }
+}
